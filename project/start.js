6b6e0bc08eedd208283a437e1381b1c35971cf3e
adin65 <46980135+adin65@users.noreply.github.com>
2023-02-22 22:13:31 +0800

    edit links

--- project/start.js
+++ project/start.js
@@ -10,37 +10,37 @@ app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
 // Set up the app's routing
-app.get('/profile', function(request, response) {
+app.get('/', function(request, response) {
   response.render('pages/index');
 });
-app.get('/profile/experience', function(request, response) {
+app.get('/experience', function(request, response) {
   response.render('pages/experience');
 });
-app.get('/profile/projects', function(request, response) {
+app.get('/projects', function(request, response) {
   response.render('pages/projects');
 });
-app.get('/profile/courses', function(request, response) {
+app.get('/courses', function(request, response) {
   response.render('pages/courses');
 });
-app.get('/profile/resume', function(request, response) {
+app.get('/resume', function(request, response) {
   response.render('pages/resume');
 });
-app.get('/profile/projects/trackTimesMacro', function(request, response) {
+app.get('/projects/trackTimesMacro', function(request, response) {
   response.render('pages/eportfolio/ttctracktimesmacro');
 });
-app.get('/profile/projects/maintenancenotif', function(request, response) {
+app.get('/projects/maintenancenotif', function(request, response) {
   response.render('pages/eportfolio/scmaintenancenotif');
 });
-app.get('/profile/projects/momeca', function(request, response) {
+app.get('/projects/momeca', function(request, response) {
   response.render('pages/eportfolio/momeca');
 });
-app.get('/profile/projects/shoop', function(request, response) {
+app.get('/projects/shoop', function(request, response) {
   response.render('pages/eportfolio/shoop');
 });
-app.get('/profile/projects/epsilon', function(request, response) {
+app.get('/projects/epsilon', function(request, response) {
   response.render('pages/eportfolio/epsilon');
 });
-app.get('/profile/projects/campfire', function(request, response) {
+app.get('/projects/campfire', function(request, response) {
   response.render('pages/eportfolio/campfire');
 });
 
